2a70c2b296451fc213a3897e79e0dc5f6fc95c9f
Josh Kaplan <nonother@gmail.com>
2017-11-14 00:22:33 -0800

    Rearchitects startup sequence of poller to not block on WeMo discovery
    The poller now starts its main loop once both switches have been discovered instead of waiting for discovery to complete. As a result discovery can be given an extended period of time (ex. 180 seconds), but if both switches are found before then the main loop will start. If both switches haven't been found after discovery then the script will terminate.

--- buanderie-poller/wemo.py
+++ buanderie-poller/wemo.py
@@ -10,6 +10,12 @@ from google.api.core.exceptions import GatewayTimeout
 
 Reading = namedtuple('Reading', ['switch', 'draw', 'timestamp'])
 
+environment = None # WeMo switch environment, used for discovery
+args = None # Parsed command line arguments
+
+# Parses the optional command line arguments
+# In the process of doing so sets the defaults for the amount of time to sleep
+# between readings and the discovery timeout to find the switches.
 def parse_args(raw_args):
 	description = "Reads Washer and Dryer Wemo Insights"
 	parser = argparse.ArgumentParser(description=description)
@@ -17,26 +23,38 @@ def parse_args(raw_args):
 		help='Sleep time between reads, in seconds')
 	parser.add_argument('-d', '--debug', dest='debug', action='store_true',
 		help='Write readings to stdout instead of saving to the database')
+	parser.add_argument('-t', '--discovery_timeout', type=int, default=180,
+		help='WeMo switch discovery timeout, in seconds')
 
 	return parser.parse_args(raw_args) 
 
+# Called when a switch is discovered.
+# Each time a switch is discovered a check is done to see if both switches have
+# been found and if so the main loop is started.
 def on_switch(switch):
-	print("Found switch: " + switch.name)
-	sys.stdout.flush()
+	print_and_flush("Found switch: " + switch.name)
 
-def on_motion(motion):
-	print("Well, this is unexpected! Found: " + motion.name)
-	sys.stdout.flush()
+	start_main_loop_if_ready()
 
-# return the devices
-def startup():
-	env = Environment(on_switch, on_motion)
-	env.start()
+# Discovers devices
+def discover_switches():
+	global environment
+	environment = Environment(on_switch)
+	environment.start()
 
-	env.discover(seconds=20)
+	print_and_flush("Starting discovery with broadcast timeout of %d seconds" % args.discovery_timeout)
 
-	return env.get_switch('Washer'), env.get_switch('Dryer')
+	environment.discover(seconds=args.discovery_timeout)
 
+	print_and_flush("Discovery broadcast complete")
+
+	# If after discovery is completed we didn't find both switches, then quit.
+	switches = environment.list_switches()
+	if 'Washer' not in switches and 'Dryer' not in switches:
+		print_and_flush("!!! Failed to find both switches before broadcast timeout of %d seconds" % args.discovery_timeout)
+		quit()
+
+# Uploads data to Google		
 def upload(client, key, reading, retries_remaining=1):
 	entity = datastore.Entity(key=key)
 	entity['switch'] = unicode(reading.switch)
@@ -71,32 +89,56 @@ def upload(client, key, reading, retries_remaining=1):
 			print("!!! GatewayTimeout. Retry limit reached :(")
 			raise
 
+# Prints data to standard out
 def debug_print(reading):
-	print '%s\t%s\t%s' % (reading.switch, reading.draw, reading.timestamp)
+	print_and_flush('%s\t%s\t%s' % (reading.switch, reading.draw, reading.timestamp))
 	sys.stdout.flush()
 
-if __name__ == '__main__':
-	print("Poller starting...")
-	sys.stdout.flush()
+# Checks if both switches are found, if so, starts main loop
+def start_main_loop_if_ready():
+	switches = environment.list_switches()
+	if 'Washer' in switches and 'Dryer' in switches:
+		washer = environment.get_switch('Washer')
+		dryer = environment.get_switch('Dryer')
+		switches = (washer, dryer)
+		main_loop(switches)
 
-	args = parse_args(sys.argv[1:])
-
-	machines = startup()
+# Main loop indefinitely reads power draw of switches and uploads the readings
+def main_loop(switches):
+	print_and_flush("Starting read and upload loop...")
 
 	client = datastore.Client()
 	key = client.key('Reading')
 
-	print("Starting read and upload loop...")
-	sys.stdout.flush()
-
 	while True:
-		for machine in machines:
-			reading = Reading(machine.name, machine.current_power, datetime.datetime.utcnow())
+		for switch in switches:
+			reading = Reading(switch.name, switch.current_power, datetime.datetime.utcnow())
 
 			if args.debug:
 				debug_print(reading)
 			else:
 				upload(client, key, reading, 2)
 			
-			machine.on()	# ensure that switch is turned back on in case of power failure
+			switch.on()	# ensure that switch is turned back on in case of power failure
 			time.sleep(args.sleep_interval)
+
+# Prints to standard out and flushes. This matters because the Linux system
+# journal (as accessed by the journalctl command) won't record system out at
+# the time print was called otherwise, which is really useful when debugging.
+def print_and_flush(text):
+	print(text)
+	sys.stdout.flush()
+
+# Main function, where it all begins
+if __name__ == '__main__':
+	print_and_flush("Poller starting...")
+
+	args = parse_args(sys.argv[1:])
+	if args.debug:
+		print_and_flush("Running in debug mode")
+	else:
+		print_and_flush("Running in production mode")
+
+	# Discovers switches. Once both switches are found the main loop will start.
+	# If neither switch is found this script will terminate.
+	discover_switches()
